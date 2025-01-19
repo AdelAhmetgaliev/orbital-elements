826eb584f744653c0dd4b188102f2384900f5f8e
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2025-01-19 10:16:41 +0300

    Added data reading

--- cmd/orbital-elements/main.go
+++ cmd/orbital-elements/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	inputFile, err := os.Open("data/example.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := inputFile.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	var inputData [6]float64
+
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Split(bufio.ScanWords)
+	for i := 0; i < 6 && scanner.Scan(); i++ {
+		value, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			panic(err)
+		}
+
+		inputData[i] = value
+	}
+
+	fmt.Println(inputData)
 }

89743666dc7d8787344cef8bc20725a81c895745
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2025-01-19 09:48:07 +0300

    Little fix

--- internal/velocity/velocity.go
+++ internal/velocity/velocity.go
@@ -12,10 +12,10 @@ func New(x float64, y float64, z float64) *Velocity {
 	return &Velocity{x, y, z}
 }
 
-func (c *Velocity) Length() float64 {
-	return math.Sqrt(c.x*c.x + c.y*c.y + c.z*c.z)
+func (v *Velocity) Length() float64 {
+	return math.Sqrt(v.x*v.x + v.y*v.y + v.z*v.z)
 }
 
-func (c *Velocity) Length2() float64 {
-	return c.x*c.x + c.y*c.y + c.z*c.z
+func (v *Velocity) Length2() float64 {
+	return v.x*v.x + v.y*v.y + v.z*v.z
 }

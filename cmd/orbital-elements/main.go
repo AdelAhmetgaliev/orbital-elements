6505ee616a60ed642a1a4fcde2c8ea0c0fc86b09
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2025-01-19 14:19:23 +0300

    Final version

--- cmd/orbital-elements/main.go
+++ cmd/orbital-elements/main.go
@@ -1,36 +1,85 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	"math"
+	"path/filepath"
+
+	"github.com/AdelAhmetgaliev/orbital-elements/internal/calculations"
+	"github.com/AdelAhmetgaliev/orbital-elements/internal/inputdata"
+	"github.com/AdelAhmetgaliev/orbital-elements/internal/vectorelement"
 )
 
+const EPS = 1e-15
+const eclipticTiltDegrees = 23.43929111
+
+func radiansToDegrees(r float64) float64 {
+	return r * (180.0 / math.Pi)
+}
+
 func main() {
-	inputFile, err := os.Open("data/example.txt")
-	if err != nil {
-		panic(err)
+	inputFilePath := filepath.FromSlash("data/input.txt")
+
+	inputCoords, inputVelocity := inputdata.Read(&inputFilePath)
+
+	distance := inputCoords.Length()
+	velocitySquare := inputVelocity.Length2()
+
+	reverseSemiMajorAxis := calculations.ReverseSemiMajorAxis(distance, velocitySquare)
+	eccentricity := calculations.Eccentricity(inputCoords, inputVelocity, reverseSemiMajorAxis)
+	eccentricAnomaly := calculations.EccentricAnomaly(inputCoords, inputVelocity, reverseSemiMajorAxis, eccentricity)
+	averageAnomaly := calculations.AverageAnomaly(eccentricAnomaly, eccentricity)
+
+	firstVectorElement := vectorelement.First(
+		inputCoords, inputVelocity, reverseSemiMajorAxis, eccentricAnomaly)
+	secondVectorElement := vectorelement.Second(
+		inputCoords, inputVelocity, reverseSemiMajorAxis, eccentricAnomaly, eccentricity)
+
+	if math.Abs(firstVectorElement.Length()-1.0) > EPS {
+		panic("Length of the first vector element is greater than 1.0")
 	}
 
-	defer func() {
-		if err := inputFile.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	var inputData [6]float64
-
-	scanner := bufio.NewScanner(inputFile)
-	scanner.Split(bufio.ScanWords)
-	for i := 0; i < 6 && scanner.Scan(); i++ {
-		value, err := strconv.ParseFloat(scanner.Text(), 64)
-		if err != nil {
-			panic(err)
-		}
-
-		inputData[i] = value
+	if math.Abs(secondVectorElement.Length()-1.0) > EPS {
+		panic("Length of the second vector element is greater than 1.0")
 	}
 
-	fmt.Println(inputData)
+	if math.Abs(vectorelement.Dot(firstVectorElement, secondVectorElement)) > EPS {
+		panic("Vector elements are not orthogonal to each other")
+	}
+
+	cose := math.Cos(math.Pi * eclipticTiltDegrees / 180.0)
+	sine := math.Sin(math.Pi * eclipticTiltDegrees / 180.0)
+
+	tempValue1 := firstVectorElement.Z*cose - firstVectorElement.Y*sine
+	tempValue2 := secondVectorElement.Z*cose - secondVectorElement.Y*sine
+
+	sinInclination := math.Sqrt(tempValue1*tempValue1 + tempValue2*tempValue2)
+
+	sinArgOfPeriapsis := tempValue1 / sinInclination
+	cosArgOfPeriapsis := tempValue2 / sinInclination
+
+	argOfPeriapsis := math.Atan2(sinArgOfPeriapsis, cosArgOfPeriapsis)
+
+	sinAscendingNode :=
+		(firstVectorElement.Y*cosArgOfPeriapsis - secondVectorElement.Y*sinArgOfPeriapsis) / cose
+	cosAscendingNode := firstVectorElement.X*cosArgOfPeriapsis - secondVectorElement.X*sinArgOfPeriapsis
+
+	ascendingNode := math.Atan2(sinAscendingNode, cosAscendingNode)
+
+	cosInclination := -(firstVectorElement.X*sinArgOfPeriapsis + secondVectorElement.X*cosArgOfPeriapsis) / sine
+
+	inclination := math.Acos(cosInclination)
+
+	argOfPeriapsisDegrees := radiansToDegrees(argOfPeriapsis)
+	ascendingNodeDegrees := radiansToDegrees(ascendingNode)
+	inclinationDegrees := radiansToDegrees(inclination)
+	averageAnomalyDegrees := radiansToDegrees(averageAnomaly)
+	semiMajorAxis := 1.0 / reverseSemiMajorAxis
+
+	fmt.Printf("ω = %.8f°\n", argOfPeriapsisDegrees)
+	fmt.Printf("Ω = %.8f°\n", ascendingNodeDegrees)
+	fmt.Printf("i = %.8f°\n", inclinationDegrees)
+	fmt.Printf("e = %.8f\n", eccentricity)
+	fmt.Printf("a = %.8f a.e.\n", semiMajorAxis)
+	fmt.Printf("M = %.8f°\n", averageAnomalyDegrees)
 }

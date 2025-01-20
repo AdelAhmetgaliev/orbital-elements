3a179ef51a813fdcfd0dfc2e4156d3c7c71b20ef
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2025-01-20 17:33:58 +0300

    Put the constants in a separate file

---
+++ internal/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+import "math"
+
+var Epsilon = math.Nextafter(1.0, 2.0) - 1.0
+
+const GravitationalConstant = 0.01720209895
+const GravitationalConstant2 = 0.0002959122082855911025
+
+const EclipticTiltDegrees = 23.43929111

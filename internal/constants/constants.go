36b9f6eb29f83038c407a48b838d7b914f447705
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2025-01-20 17:48:58 +0300

    Updated the constant

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,8 +1,6 @@
 package constants
 
-import "math"
-
-var Epsilon = math.Nextafter(1.0, 2.0) - 1.0
+const Epsilon = 2.220446049250313e-16
 
 const GravitationalConstant = 0.01720209895
 const GravitationalConstant2 = 0.0002959122082855911025
